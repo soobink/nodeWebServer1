791c465448982dcfef476d986fc268797c366fcf
Soobin Kang <SoobinK@multotec.au>
2018-06-21 22:00:53 +1000

    Initial commit

---
+++ server.js
@@ -0,0 +1,75 @@
+//install npm install hbs@4.0.0 --save
+
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+
+app.use((req, res, next)=>{
+	var now = new Date().toString();
+	var log = `${now}: ${req.method}: ${req.url}`
+	
+	console.log(log);
+	fs.appendFile('server.log', log + '\n', (err) =>{
+		if(err){
+			console.log('Unable to append to server.log;
+		}
+	});
+	//console.log(`${now}: ${req.method}: ${req.url}`);
+	next();
+});
+
+app.use((req,res,next)=>{
+	res.render('maintenance.hbs');
+});
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', ()=>{
+	return new Date().getFullYear()
+});
+
+hbs.registerHelper('screamIt', (text)=>{
+	return text.toUpperCase()
+});
+
+app.get('/', (req, res)=>{
+	//res.send('<h1>Hello Express!</h1>');
+	// res.send({
+		// name: 'Hugo',
+		// likes: [
+			// 'Lego',
+			// 'soccer'
+		
+		// ]
+	res.render('home.hbs',{
+	pageTile: 'Home Page',
+	currentYear: new Date().getFullYear()
+	welcomeMessage: 'Welcome to Soobin\'s website'
+	});
+});
+
+app.get('/about', (req, res) =>{
+	//res.send('About Page.');
+	res.render('about.hbs');
+	pageTile: 'About Page',
+	currentYear: new Date().getFullYear()
+});
+
+app.get('/bad',(req, res)=>{
+	res.send({
+		errorMessage: 'unable to handle request.'
+	});
+});
+
+
+
+app.listen(3000, ()=>{
+	console.log('Server is up on port 3000')
+});
